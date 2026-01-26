cbee9857bbe7598a800c30ae647d5c2f286a5040
Jigyasa-prep <dasjigyasa1437@gmail.com>
2026-01-26 15:35:19 +0530

    patterns

--- DSA/patterns.cpp
+++ DSA/patterns.cpp
@@ -1,9 +1,9 @@
 //square patterns using loop
 #include<bits/stdc++.h>
 using namespace std;
-void pattern1(int N){//for rows
-    for(int i=0;i<N;i++){//for columns
-        for(int j=0;j<N;j++){
+void pattern1(int n){//for rows
+    for(int i=0;i<n;i++){//for columns
+        for(int j=0;j<n;j++){
             cout<<"* ";
         }
         cout<<endl;
@@ -11,8 +11,8 @@ void pattern1(int N){//for rows
 }
 //Right angled triangle pattern
 //no of * increase one by one in line by line row i contain i+1 *
-void pattern2(int N){
-    for(int i=0;i<N;i++){
+void pattern2(int n){
+    for(int i=0;i<n;i++){
         for(int j=0;j<=i;j++){
             cout<<"* ";
         }
@@ -23,8 +23,8 @@ void pattern2(int N){
 //each row contain numbers starting from 1 to row number
 //outer loop 1 to n
 //inner loop 1 to i print j
-void pattern3(int N){
-    for(int i=0;i<=N;i++){
+void pattern3(int n){
+    for(int i=0;i<=n;i++){
         for(int j=0;j<=i;j++){
             cout<<j<<" ";
         }
@@ -35,8 +35,8 @@ void pattern3(int N){
 //each row contain numbers starting from 1 to row number
 //outer loop 1 to n
 //inner loop 1 to i print i
-void pattern4(int N){
-    for(int i=0;i<=N;i++){
+void pattern4(int n){
+    for(int i=0;i<=n;i++){
         for(int j=0;j<=i;j++){
             cout<<i<<" ";
         }
@@ -46,20 +46,29 @@ void pattern4(int N){
 //inverted right angled triangle of *
 //outer loop 0 to n
 //inner loop n to i j--
-void pattern5(int N){
-    for(int i=0;i<N;i++){
-        for(int j=N;j>i;j--){
+void pattern5(int n){
+    for(int i=0;i<n;i++){
+        for(int j=n;j>i;j--){
             cout<<"* ";
         }
         cout<<endl;
     }
 }
+void pattern6(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i+1;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+};
 int main(){
-    int N=5;
-    pattern1(N);
-    pattern2(N);
-    pattern3(N);
-    pattern4(N);
-    pattern5(N);
+    int n=4;
+    pattern1(n);
+    pattern2(n);
+    pattern3(n);
+    pattern4(n);
+    pattern5(n);
+    pattern6(n);
     return 0;
 }
