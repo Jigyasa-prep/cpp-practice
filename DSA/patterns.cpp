ca3864c57edbb8597b17ba5b4ba71a465cead93a
Jigyasa-prep <dasjigyasa1437@gmail.com>
2026-01-27 01:49:58 +0530

    patternpractice

--- DSA/patterns.cpp
+++ DSA/patterns.cpp
@@ -62,6 +62,71 @@ void pattern6(int n){
         cout<<endl;
     }
 };
+void pattern7(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<2*i+1;j++){
+            cout<<"*";
+        }
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+    cout<<endl;
+    }
+};
+void pattern8(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<2*n-(2*i+1);j++){
+            cout<<"*";
+        }
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+    cout<<endl;
+    }
+    };
+void pattern10(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=0;i<n-1;i++){
+        for(int j=n-1;j>i;j--){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+};
+void pattern11(int n) {
+    // First row starts by printing a single 1.
+    int start = 1;
+    
+    // Outer loop for the number of rows
+    for (int i = 0; i < n; i++) {
+        // If the row index is even, print 1 first in that row
+        if (i % 2 == 0) start = 1;
+        
+        // If the row index is odd, print 0 first in that row
+        else start = 0;
+        
+        // Inner loop to print alternating 1's and 0's in each row
+        for (int j = 0; j <= i; j++) {
+            cout << start;
+            // Alternate between 1 and 0
+            start = 1 - start;
+        }
+
+        // Move to the next line after each row is printed
+        cout << endl;
+    }
+};
 int main(){
     int n=4;
     pattern1(n);
@@ -70,5 +135,9 @@ int main(){
     pattern4(n);
     pattern5(n);
     pattern6(n);
+    pattern7(n);
+    pattern8(n);
+    pattern10(n);
+    pattern11(n);
     return 0;
 }
